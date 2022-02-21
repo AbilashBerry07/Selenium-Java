3450a2ae26a7b62f04a19bd7fede007cf6a3d6a3
AbilashBerry07 <99703090+AbilashBerry07@users.noreply.github.com>
2022-02-21 21:24:04 +0530

    Add files via upload

---
+++ FacebookLogin.java
@@ -0,0 +1,25 @@
+package week2.day5;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FacebookLogin {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
+		driver.findElement(By.xpath("//a[@class='_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy']")).click();
+		driver.findElement(By.xpath("//input[@name='firstname']")).sendKeys("Abilash");
+		driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys("Berry");
+		driver.findElement(By.xpath("//input[@class='email']")).sendKeys("berry.abilash@gmail.com");
+		
+	}
+
+}
